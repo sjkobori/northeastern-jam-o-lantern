b8f95d89ddb2b337f280afa97e00d313e0c8de68
Sebastian Kobori <sjkobori@gmail.com>
2021-10-28 22:16:21 -0400

    fixed walljump direction

--- Assets/Scripts/Movement/States/WallJumping.cs
+++ Assets/Scripts/Movement/States/WallJumping.cs
@@ -7,16 +7,21 @@ public class WallJumping : MovementState
 {
     [SerializeField] private MovementState freefallState;
 
-    private float _jumpStart;
-    private float maxJumpHold = 0.15f;
+    //private float _jumpStart;
+    //private float maxJumpHold = 0.15f;
+    private float _jumpTime;
+    private float maxJumpTime = 0.75f;
+    private float minJumpTime = 0.2f;
 
     public override void Enter(GameObject gameObject)
     {
         base.Enter(gameObject);
-        _jumpStart = Time.time;
+        _jumpTime = 0;
         gameObject.GetComponent<SpriteRenderer>().color = Color.cyan;
         Rigidbody2D rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
-        rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x + 20, rigidbody2D.velocity.y + 10);
+        PlayerMovementController pmc = gameObject.GetComponent<PlayerMovementController>();
+        int jumpX = pmc.wallSideLeft ? 10 : -10;
+        rigidbody2D.velocity = new Vector2( jumpX,  10);
         
     }
 
@@ -24,8 +29,12 @@ public class WallJumping : MovementState
     public override MovementState UpdateLogic(GameObject gameObject)
     {
         PlayerMovementController pmc = gameObject.GetComponent<PlayerMovementController>();
-        if (!pmc.jump || Time.time - _jumpStart >= maxJumpHold)
+        pmc.horizontalAxis = 0;
+        _jumpTime += Time.deltaTime;
+        Debug.Log("jumptime is:" + _jumpTime);
+        if ((!pmc.jump && _jumpTime > minJumpTime) || _jumpTime >= maxJumpTime)
         {
+            Debug.Log("Going to freefall, reache max hold time:" + maxJumpTime);
             return freefallState;
         }
 
--- Assets/Scripts/Player/EnemyAIController.cs
+++ Assets/Scripts/Player/EnemyAIController.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementController : MonoBehaviour
+public class EnemyAIController : MonoBehaviour
 {
     public Transform groundPos;
     public Transform wallPos;
     public LayerMask groundLayer;
-    public FloatReference initialMoveSpeed;
-    public FloatReference maxMoveSpeed;
-    public FloatReference gravity;
-    public FloatReference jumpGravity;
-    public FloatReference airFriction;
-    public FloatReference groundFriction;
+    public EnemyStats stats;
+
 
     [HideInInspector]
     public float moveSpeed;
@@ -37,17 +33,17 @@ public class PlayerMovementController : MonoBehaviour
 
     private void Awake()
     {
-        moveSpeed = initialMoveSpeed.value;
+        moveSpeed = stats.moveSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
         horizontalAxis = Input.GetAxis("Horizontal");
         verticalAxis = Input.GetAxis("Vertical");
         jump = Input.GetButton("Jump");
-        
+        */
         grounded = Physics2D.OverlapBoxAll(groundPos.position, new Vector2(.5f, 0.1f), 0, groundLayer).Length > 0;
         var results = Physics2D.OverlapBoxAll(wallPos.position, new Vector2(1.1f, .5f), 0, groundLayer);
         walled = results.Length > 0;

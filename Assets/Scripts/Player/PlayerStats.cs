0cf8a55c5230d75a90931f5ac23f2ca375831384
Sebastian Kobori <sjkobori@gmail.com>
2021-11-04 20:31:09 -0400

    Added Player stats to access globally

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerStats", menuName = "ScriptableObjects/PlayerStats", order = 1)]
+public class PlayerStats : ScriptableObject
+{
+    [SerializeField] 
+    private int baseHealth;            
+    [HideInInspector]
+    public int health;
+    [SerializeField]
+    private int baseAmmo;
+    [HideInInspector]
+    public int ammo;
+
+    private void OnEnable()
+    {
+        health = baseHealth;
+        ammo = baseAmmo;
+    }
+}
